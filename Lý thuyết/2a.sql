7057635af30eb9937cb150aa7144b05c6bbcadcc
Levytan <42597804+Levytan@users.noreply.github.com>
2024-02-23 14:26:08 +0700

    Add files via upload

---
+++ Lý thuyết/2a.sql
@@ -0,0 +1,28 @@
+﻿-- tạo mới
+create database [học kỳ 2a]
+
+--chuyển qua [học kỳ 2a] để sử dụng
+use [học kỳ 2a]
+
+-- tạo bảng
+create table [sinh viên] (
+	[mã số] int,				-- lưu trữ số
+	[họ và tên] nvarchar(200),	-- lưu trữ chữ
+	[ngày sinh] date			-- lưu trữ thời gian
+)
+
+
+-- so sánh char(n) với varchar(n)
+select replace(cast('abc' as char(10)), ' ', '*')
+select replace(cast('abc' as varchar(10)), ' ', '*')
+
+-- số ký tự lớn hơn giới hạn
+select cast('abcd' as varchar(2))
+
+-- so sánh varchar(n) với nvarchar(n)
+select cast(N'こんにち' as varchar(5))
+select cast(N'こんにち' as nvarchar(5))
+
+-- so sánh datetime với datetime2
+select cast('2024-2-23 0:0:0.005' as datetime)
+select cast('2024-2-23 0:0:0.005' as datetime2(3))
